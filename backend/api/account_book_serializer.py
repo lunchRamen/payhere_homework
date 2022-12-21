4341df55ab78478544975f5be465901e00dace94
lunchRamen <bloom6561@naver.com>
2022-12-22 03:35:41 +0900

    feat(account_book_serializer): 유저 가계부 생성 시리얼라이저 추가

---
+++ backend/api/account_book_serializer.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+from .models import AccountBook
+
+class AccountBookListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AccountBook
+        fields = '__all__'
+
+class AccountBookGetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AccountBook
+        fields = '__all__'
+
+class AccountBookCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AccountBook
+        fields = '__all__'
+
+class AccountBookUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AccountBook
+        fields = '__all__'
+class AccountBookDestroySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AccountBook
+        fields = '__all__'
