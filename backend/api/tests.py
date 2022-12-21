b65fd93b5b4f53f38c51290ccd5c86c533b6ce98
lunchRamen <bloom6561@naver.com>
2022-12-21 16:49:58 +0900

    test(signup): 유저 생성 실패 케이스 추가 구현

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -21,3 +21,21 @@ class UserTestCase(TestCase):
     def test_유저_생성_실패_정보부족(self):
         res = self.client.post('http://127.0.0.1:8000/api/users/signup/')
         self.assertEqual(res.status_code, 400)
+
+    def test_유저_생성_실패_이메일형식(self):
+        data = {
+            "email": "test",
+            "password1": "asdff1234",
+            "password2": "asdff1234"
+        }
+        res = self.client.post('http://127.0.0.1:8000/api/users/signup/',data)
+        self.assertEqual(res.status_code, 400)
+    
+    def test_유저_생성_실패_비밀번호형식(self):
+        data = {
+            "email": "test@gmail.com",
+            "password1": "1234",
+            "password2": "1234"
+        }
+        res = self.client.post('http://127.0.0.1:8000/api/users/signup/',data)
+        self.assertEqual(res.status_code, 400)
