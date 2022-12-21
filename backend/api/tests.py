6055b1b97c24e7f267abb8f0314c0c3c7b8b3413
lunchRamen <bloom6561@naver.com>
2022-12-21 18:55:16 +0900

    test(login,logout): 로그인 성공/실패, 로그아웃 성공 케이스 테스트 구현.

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -2,11 +2,12 @@
 from django.test import TestCase
 # from rest_framework.response import Response
 from rest_framework.test import APIClient
+from .models import User
 
 # from django.urls import reverse
 
 
-class UserTestCase(TestCase):
+class UserSignupTestCase(TestCase):
     client_calss = APIClient
 
     def test_유저_생성_성공(self):
@@ -39,3 +40,48 @@ class UserTestCase(TestCase):
         }
         res = self.client.post('http://127.0.0.1:8000/api/users/signup/',data)
         self.assertEqual(res.status_code, 400)
+
+    
+class UserLoginTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(
+            email = "test@gmail.com"
+        )
+        self.user.set_password("asdff1234")
+        self.user.save()
+
+    def test_유저_로그인_성공(self):
+        data = {
+            "email": "test@gmail.com",
+            "password": "asdff1234"
+        }
+        res = self.client.post('http://127.0.0.1:8000/api/users/login/',data)
+        self.assertEqual(res.status_code , 200)
+    
+    def test_유저_로그인_실패_이메일(self):
+        data = {
+            "email": "test2@gmail.com",
+            "password": "asdff1234"
+        }
+        res = self.client.post('http://127.0.0.1:8000/api/users/login/',data)
+        self.assertEqual(res.status_code , 400)
+    
+    def test_유저_로그인_실패_비밀번호(self):
+        data = {
+            "email": "test2@gmail.com",
+            "password": "asdf234"
+        }
+        res = self.client.post('http://127.0.0.1:8000/api/users/login/',data)
+        self.assertEqual(res.status_code , 400)
+
+class UserLogoutTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(
+            email = "test@gmail.com"
+        )
+        self.user.set_password("asdff1234")
+        self.user.save()
+    
+    def test_유저_로그아웃_성공(self):
+        res = self.client.post('http://127.0.0.1:8000/api/users/logout/')
+        self.assertEqual(res.status_code, 200)
